11d75d6d9d2f0a2c2469962e47923a8628b0819d
daniel-turu <danielturu30@gmail.com>
2024-06-06 15:40:28 +0100

    added support-nav

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -2,9 +2,9 @@ import { HiX } from "react-icons/hi";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./style.css";
-import { useEffect,  useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as ScrollLink } from "react-scroll";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 const Navbar = () => {
   const [toggleIcon, setToggleIcon] = useState(false);
   const homePageUrl = "/";
@@ -19,15 +19,14 @@ const Navbar = () => {
 
   const scrollOffset = -90;
 
-
-useEffect(() => {
+  useEffect(() => {
     // Check if URL contains a hash
     if (location.hash) {
       // Get the element corresponding to the hash
       const targetElement = document.querySelector(location.hash);
       if (targetElement) {
         // Scroll smoothly to the target element
-        targetElement.scrollIntoView({ behavior: 'smooth' });
+        targetElement.scrollIntoView({ behavior: "smooth" });
       }
     }
   }, [location.hash]);
@@ -56,10 +55,9 @@ useEffect(() => {
           )}
         </div>
         <ul id="ul" className={toggleIcon ? "active" : ""}>
-         
           <li>
             <Link
-            className="no-underline text-current"
+              className="no-underline text-current"
               to={`${homePageUrl}#about-section`}
               onClick={handleToggleItem}
             >
@@ -68,7 +66,7 @@ useEffect(() => {
           </li>
           <li>
             <Link
-            className="no-underline text-current"
+              className="no-underline text-current"
               to={`${homePageUrl}#our-feature-section`}
               onClick={handleToggleItem}
             >
@@ -77,14 +75,49 @@ useEffect(() => {
           </li>
           <li>
             <Link
-            className="no-underline text-current"
+              className="no-underline text-current"
               to={`${homePageUrl}#how-it-works-section`}
               onClick={handleToggleItem}
             >
               How it works
             </Link>
           </li>
-          <li>
+
+          <li className="relative group">
+            <span className="cursor-pointer no-underline text-current">
+              Support
+            </span>
+            <ul className="absolute hidden group-hover:block bg-white rounded-md shadow-md z-10 pb-3 text-left w-[120px]">
+              <li>
+                <Link
+                  className="no-underline text-2xl block  py-3 text-gray-700 hover:text-[#0066f5]"
+                  to="/safety"
+                >
+                  Safety
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="no-underline text-2xl block  py-3 text-gray-700 hover:text-[#0066f5]"
+                  to="/guides"
+                >
+                  Guides
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className="no-underline text-2xl py-3 text-gray-700 hover:text-[#0066f5]"
+                  to={`#footer`}
+                  onClick={handleToggleItem}
+                >
+                  Contact-Us
+                </Link>
+              </li>
+            </ul>
+          </li>
+
+          {/* <li>
             <Link className="no-underline text-current" to="/guides">
               Guides
             </Link>
@@ -94,8 +127,7 @@ useEffect(() => {
             <Link className="no-underline text-current" to="/safety">
               Safety
             </Link>
-          </li>
-
+          </li> */}
 
           <ScrollLink
             to="download-section"
