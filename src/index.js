d0da5338a4f740c7460bb78e8f0a8c0ba586bebb
novertech411 <novertech4@gmail.com>
2025-01-03 11:56:45 +0100

    40% done with the carosel

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import reportWebVitals from "./reportWebVitals";
 
